6cb6f1f2686a77a321247f421ce4059283ac04f1
Penguin Sleep <1511467743@qq.com>
2024-10-15 23:21:08 +0800

    add two devices results

--- main.cu
+++ main.cu
@@ -9,15 +9,12 @@
 using Dvector = thrust::device_vector<double>;
 using Hvector = thrust::host_vector<double>;
 
-template <typename T>
-std::ostream &operator<<(std::ostream &os, const thrust::host_vector<T> &h_vec)
-{
+template<typename T>
+std::ostream &operator<<(std::ostream &os, const thrust::host_vector<T> &h_vec) {
     os << "[";
-    for (size_t i = 0; i < h_vec.size(); ++i)
-    {
+    for (size_t i = 0; i < h_vec.size(); ++i) {
         os << h_vec[i];
-        if (i != h_vec.size() - 1)
-        {
+        if (i != h_vec.size() - 1) {
             os << ", ";
         }
     }
@@ -26,51 +23,43 @@ std::ostream &operator<<(std::ostream &os, const thrust::host_vector<T> &h_vec)
 }
 
 // cal core
-struct distance
-{
+struct distance {
     int N{};
     double *pD_vec;
 
-    distance(int n, Dvector &d_vec) : N(n), pD_vec(thrust::raw_pointer_cast(d_vec.data())) {}
+    distance(const int n, Dvector &d_vec) : N(n), pD_vec(thrust::raw_pointer_cast(d_vec.data())) {
+    }
 
-    __device__ double operator()(const int &idx)
-    {
+    __device__ double operator()(const int &idx) const {
         // idx: 1 -> N*N
-        int i = idx / N;
-        int j = idx % N;
+        const int i = idx / N;
+        const int j = idx % N;
         return ((pD_vec[i] - pD_vec[j]) * (pD_vec[i] - pD_vec[j]) +
                 (pD_vec[i + N] - pD_vec[j + N]) * (pD_vec[i + N] - pD_vec[j + N]) +
                 (pD_vec[i + 2 * N] - pD_vec[j + 2 * N]) * (pD_vec[i + 2 * N] - pD_vec[j + 2 * N]));
     }
 };
 
-void save(Hvector const &v, std::string filename)
-{
-    std::ofstream out(filename, std::ios::binary);
-    if (out.is_open())
-    {
-        size_t size = v.size();
+void save(Hvector const &v, const std::string &filename) {
+    if (std::ofstream out(filename, std::ios::binary); out.is_open()) {
+        const size_t size = v.size();
         out.write(reinterpret_cast<const char *>(thrust::raw_pointer_cast(v.data())), size * sizeof(double));
         out.close();
-    }
-    else
-    {
+    } else {
         std::cerr << "Can't open file " << filename << "\n";
     }
 }
 
-int main()
-{
-    int N = 1000;
+int main() {
+    // N 在 4e4往上会出现int溢出的问题还需要处理
+    constexpr int N = 1000;
 
     // Generate 3N random numbers on device.
     thrust::default_random_engine rng(1332);
     thrust::uniform_real_distribution<double> distribute(-50.0, 50.0);
     Hvector h_vec(3 * N);
-    thrust::generate(h_vec.begin(), h_vec.end(), [&]
-                     { return distribute(rng); });
+    thrust::generate(h_vec.begin(), h_vec.end(), [&] { return distribute(rng); });
 
-    save(h_vec, "input.dat");
 
     // x0,x1,x2...,y0,y1...,z0,z1...,zN  3*N
     Dvector d_vec = h_vec;
@@ -82,26 +71,31 @@ int main()
     thrust::transform(thrust::counting_iterator<int>(0),
                       thrust::counting_iterator<int>(N * N), d_dis2.begin(), distance(N, d_vec));
 
-    checkCudaErrors(cudaDeviceSynchronize()); // spend more time than core
+    checkCudaErrors(cudaDeviceSynchronize());
+
     TOCK(distanceCore);
 
-    Hvector h_dis2 = d_dis2;
-    save(h_dis2, "output.dat");
+    Hvector h_dis2 = d_dis2; // 考虑这个拷贝耗时会大大增加，瓶颈主要是数据IO
 
     // std::cout << h_vec << std::endl;
     // std::cout << h_dis2 << std::endl;
 
     // check errors
-    TICK(errorCheck);
-    for (int id = 0; id < h_dis2.size(); id++)
-    {
-        int i = id / N;
-        int j = id % N;
-        double h_res = (h_vec[i] - h_vec[j]) * (h_vec[i] - h_vec[j]) +
-                       (h_vec[i + N] - h_vec[j + N]) * (h_vec[i + N] - h_vec[j + N]) +
-                       (h_vec[i + 2 * N] - h_vec[j + 2 * N]) * (h_vec[i + 2 * N] - h_vec[j + 2 * N]);
-        if ((h_res - h_dis2[id]) > 1e-5)
-            printf("error id %d\n, cpu res %.4f, gpu res %.4f\n", id, h_res, h_dis2[id]);
+    if (N <= 1000) {
+        save(h_vec, "input.dat");
+        save(h_dis2, "output.dat");
+        TICK(errorCheck);
+        for (int id = 0; id < h_dis2.size(); id++) {
+            const int i = id / N;
+            const int j = id % N;
+            const double h_res = (h_vec[i] - h_vec[j]) * (h_vec[i] - h_vec[j]) +
+                                 (h_vec[i + N] - h_vec[j + N]) * (h_vec[i + N] - h_vec[j + N]) +
+                                 (h_vec[i + 2 * N] - h_vec[j + 2 * N]) * (h_vec[i + 2 * N] - h_vec[j + 2 * N]);
+            if ((h_res - h_dis2[id]) > 1e-5)
+                printf("error id %d\n, cpu res %.4f, gpu res %.4f\n", id, h_res, h_dis2[id]);
+        }
+        TOCK(errorCheck);
+    } else {
+        std::cout << "N is too large, skip save and check\n";
     }
-    TOCK(errorCheck);
 }
--- main.py
+++ main.py
@@ -4,17 +4,25 @@ from scipy.spatial import distance_matrix
 from scipy.spatial.distance import cdist
 
 N = 1000
-input_raw = np.fromfile("input.dat", dtype=np.float64)
+if N <= 1000:
+    input_raw = np.fromfile("input.dat", dtype=np.float64)
 
-input = input_raw.reshape(3, N).T
+    in_mat = input_raw.reshape(3, N).T
 
-start_time = time.time()
-output = cdist(input, input, "euclidean")  # distance_matrix(a, b)
-end_time = time.time()
-print(end_time - start_time)
+    start_time = time.time()
+    output = cdist(in_mat, in_mat, "euclidean")  # distance_matrix(a, b)
+    end_time = time.time()
+    print(f"use: {end_time - start_time}")
 
-output2 = output * output
-cuda_out2 = np.fromfile("output.dat", dtype=np.float64).reshape(N, N)
+    output2 = output * output
+    cuda_out2 = np.fromfile("output.dat", dtype=np.float64).reshape(N, N)
 
-print(f"python: {np.mean(output2)}")
-print(f"cuda: {np.mean(cuda_out2)}")
+    print(f"python: {np.mean(output2)}")
+    print(f"cuda: {np.mean(cuda_out2)}")
+else:
+    print("N is too large, use random func.")
+    in_mat = np.random.rand(N, 3)
+    start_time = time.time()
+    output = cdist(in_mat, in_mat, "euclidean")  # distance_matrix(a, b)
+    end_time = time.time()
+    print(f"use: {end_time - start_time}")
